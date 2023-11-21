a0eba65ebe6309dfe212d8db8d2fabe3f47698b8
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-11-21 15:47:59 +0100

    Modify schema.sql

--- schema.sql
+++ schema.sql
@@ -8,3 +8,20 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
     ADD COLUMN species VARCHAR(255);
+
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+ALTER TABLE animals
+    DROP COLUMN species,
+    ADD COLUMN species_id INTEGER,
+    ADD COLUMN owner_id INTEGER;
