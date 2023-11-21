6c795f80b4deccf401959567ca28a75fdc5267b0
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-11-21 16:02:07 +0100

    Add schema

--- schema.sql
+++ schema.sql
@@ -25,3 +25,27 @@ ALTER TABLE animals
     DROP COLUMN species,
     ADD COLUMN species_id INTEGER,
     ADD COLUMN owner_id INTEGER;
+
+    ALTER TABLE animals
+    ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id),
+    ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INTEGER,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+    vet_id INTEGER REFERENCES vets(id),
+    species_id INTEGER REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
+);
+
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    animal_id INTEGER REFERENCES animals(id),
+    vet_id INTEGER REFERENCES vets(id),
+    date_of_visit DATE
+);
